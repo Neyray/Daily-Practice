bce2c3b4a97b48cdbea02b2f2c7a7e02b1b10dce
Jerico <zjr19374507113@outlook.com>
2025-11-09 23:12:55 +0800

    上传了高程实验报告

---
+++ 高程/实验报告/chapter2/chapter2/chapter2.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int number = 42; // 预设数字
+
+    //while循环
+    int guess;
+    cout << "猜数字游戏开始！数字在1~100之间。" << endl;
+
+    while (true) {
+        cout << "请输入你猜的数字: ";
+        cin >> guess;
+        if (guess == number) {
+            cout << "恭喜你猜对了！" << endl;
+            break;
+        }
+        else if (guess < number) {
+            cout << "猜小了！" << endl;
+        }
+        else {
+            cout << "猜大了！" << endl;
+        }
+    }
+
+
+    //for循环
+    for (;;) {
+        cout << "请输入你猜的数字: ";
+        cin >> guess;
+        if (guess == number) {
+            cout << "恭喜你猜对了！" << endl;
+            break;
+        }
+        else if (guess < number) {
+            cout << "猜小了！" << endl;
+        }
+        else {
+            cout << "猜大了！" << endl;
+        }
+    }
+
+    //do_while循环
+    do {
+        cout << "请输入你猜的数字: ";
+        cin >> guess;
+        if (guess == number) {
+            cout << "恭喜你猜对了！" << endl;
+            break;
+        }
+        else if (guess < number) {
+            cout << "猜小了！" << endl;
+        }
+        else {
+            cout << "猜大了！" << endl;
+        }
+    } while (true);
+
+
+    return 0;
+}
+
